c51ef440caf440de28a1bd758b4a647b8565a806
Taylor Woodman <taylor.woodman711@gmail.com>
2019-11-16 21:21:21 -0700

    css slightly

--- src/invoice.js
+++ src/invoice.js
@@ -5,34 +5,34 @@ import BillTo from "./billTo"
 
 class Invoice extends React.Component {
    
-        state = {
-            invNum: '',
-            date: ''
-        }
-    
+		state = {
+				invNum: '',
+				date: ''
+		}
 
-    render(){
-        return (
-            <div className='invoice-page'>
-                <div className="top">
-                    <div className="info">
-                        <div className="company-name">JNH Construction</div>
-                        <div className="address">Address, city, state, zip, phone</div>
-                    </div>
-                    <div className="inv-date">
-                        <div className="inv-num"></div>
-                        <div className="date"></div>
-                    </div>
-                </div>
-                <div className="mid">
-                    <BillTo />
-                </div>
-                <div className="bottom">
-                 <Description />
-                </div>
-            </div>
-        )
-    }
+
+render(){
+		return (
+				<div className='invoice-page'>
+						<div className="top">
+								<div className="info">
+										<div className="company-name">JNH Construction</div>
+										<div className="address">Address, city, state, zip, phone</div>
+								</div>
+								<div className="inv-date">
+										<div className="inv-num"></div>
+										<div className="date"></div>
+								</div>
+						</div>
+						<div className="mid">
+								<BillTo />
+						</div>
+						<div className="bottom">
+							<Description />
+						</div>
+				</div>
+		)
+}
 }
 
 export default Invoice
